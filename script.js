0de7502ab43575c7796c830bf7849fae967061d2
hyeryungkim <hyeryungkim.kr@gmail.com>
2026-07-27 01:16:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -24,32 +24,32 @@ const galleryData = [
         title: 'AZIT _ Deep Deep Deep 4',
         desc: '25 x 25 cm\nPorcelain, mixed media on wood panel'
     },
-     {
+    {
         src: 'images/005.jpg',
         title: 'AZIT _ Deep Deep Deep 5',
         desc: '25 x 25 cm\nPorcelain, mixed media on wood panel'
     },
- {
+    {
         src: 'images/006.jpg',
         title: 'AZIT _ Tranquil shadow 1',
         desc: '31.8 x 31.8 cm\nPorcelain, mixed media on wood panel'
     },
- {
+    {
         src: 'images/007.jpg',
-        title: 'AZIT _ Tranquil shadow 1 (Ditail)',
+        title: 'AZIT _ Tranquil shadow 1 (Detail)',
         desc: '31.8 x 31.8 cm\nPorcelain, mixed media on wood panel'
     },
- {
+    {
         src: 'images/008.jpg',
         title: 'AZIT _ Tranquil shadow 2',
         desc: '31.8 x 31.8 cm\nPorcelain, mixed media on wood panel'
     },
- {
+    {
         src: 'images/009.jpg',
-        title: 'AZIT _ Tranquil shadow 2 (Ditail)',
+        title: 'AZIT _ Tranquil shadow 2 (Detail)',
         desc: '31.8 x 31.8 cm\nPorcelain, mixed media on wood panel'
     },
- {
+    {
         src: 'images/010.jpg',
         title: 'AZIT _ Tranquil shadow 3',
         desc: '31.8 x 31.8 cm\nPorcelain, mixed media on wood panel'

b087cc9ceba9bd8558cb06e43d4358047c97df72
hyeryungkim <hyeryungkim.kr@gmail.com>
2026-07-26 23:37:06 +0900

    Add new image objects to script.js

--- script.js
+++ script.js
@@ -24,4 +24,14 @@ const galleryData = [
         title: 'AZIT _ Deep Deep Deep 4',
         desc: '25 x 25 cm\nPorcelain, mixed media on wood panel'
     }
+     {
+        src: 'images/005.jpg',
+        title: 'AZIT _ Deep Deep Deep 5',
+        desc: '25 x 25 cm\nPorcelain, mixed media on wood panel'
+    }
+ {
+        src: 'images/006.jpg',
+        title: 'AZIT _ Tranquil shadow 1',
+        desc: '31.8 x 31.8 cm\nPorcelain, mixed media on wood panel'
+    }
 ];
